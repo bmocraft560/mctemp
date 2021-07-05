169460bce75df7f3f31e3fc3ba4b09dfa3b1a6d7
bmocraft560 <58183106+bmocraft560@users.noreply.github.com>
2021-07-05 19:30:07 +0300

    version 1.0
    A plugin that returns the temperature of a block for minecraft when you click on it with a stick.

---
+++ MCTemp/src/lkpot/main/HardSex.java
@@ -0,0 +1,15 @@
+package lkpot.main;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HardSex extends JavaPlugin {
+    public void onEnable () {
+        Bukkit.getPluginManager().registerEvents(new Handler(),this);
+        getLogger().info("Plugin was enabled.");
+    }
+
+    public void onDisable () {
+        getLogger().info("Plugin was disabled.");
+    }
+}
